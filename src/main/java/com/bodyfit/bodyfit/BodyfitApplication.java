bddf7259675845f5c2ba9049da77f3f6212db8be
이시은 <tldms0137@naver.com>
2021-08-23 10:03:59 +0900

    DBConnection 테스트 완료

---
+++ src/main/java/com/bodyfit/bodyfit/BodyfitApplication.java
@@ -0,0 +1,13 @@
+package com.bodyfit.bodyfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodyfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodyfitApplication.class, args);
+	}
+
+}
