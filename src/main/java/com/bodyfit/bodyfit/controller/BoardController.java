e07de6eb5479bf8a9a731a92be213e166183b2e5
이시은 <tldms0137@naver.com>
2021-09-04 00:12:01 +0900

    게시글 crud 수정

--- src/main/java/com/bodyfit/bodyfit/controller/BoardController.java
+++ src/main/java/com/bodyfit/bodyfit/controller/BoardController.java
@@ -24,67 +24,70 @@ public class BoardController {
 	@Autowired
 	private BoardTypeService boardTypeService;
 
-//====================================================================================================================================== 게시글 삭제
-	@PostMapping(value = "/member/boardDelete")
+//==================================================================================================================== 게시글 삭제
+	@PostMapping(value = "/board/boardDelete")
 	public String boardDelete(@RequestParam(value = "bno", required = false) Integer bno) throws Exception {
 		System.out.println(bno);
 		if (bno == null) {
 			// TODO => 올바르지 않은 접근이라는 메시지를 전달하고, 게시글 리스트로 리다이렉트
-			return "redirect:/member/boardList";
+			return "redirect:/board/boardList";
 		}
 		boardService.deleteBoard(bno);
-		return "redirect:/member/boardList";
+		return "redirect:/board/boardList";
+	}
+//==================================================================================================================== 게시글 수정
+	@PostMapping(value="/board/boardUpdate")
+	public String boardUpdate(@ModelAttribute("params") BoardDTO params,BoardDTO boardDTO) throws Exception{		
+		boardService.updateBoard(boardDTO);
+		
+		return "redirect:/board/boardView?currentPageNo="+params.getCurrentPageNo()+"&bno="+boardDTO.getBno();
 	}
 	
-//====================================================================================================================================== 게시글 상세보기	
-	@GetMapping(value="/member/boardView")
-	public String boardView(@RequestParam(value="bno", required=false) Integer bno, Model model) throws Exception{
-		//System.out.println(bno);
+	@GetMapping(value="/board/boardUpdate")
+	public String boardUpdate(@RequestParam(value="bno", required=false) Integer bno, Model model) throws Exception{
+		System.out.println("bno====================="+bno);
 		if(bno==null) {
-			return "redirect:/member/boardList";
+			return "redirect:/board/boardList";
 		}
 		BoardDTO board = boardService.selectBoardDetail(bno);
 		model.addAttribute("board", board);
-		return "member/boardView";
+		return "board/boardUpdate";
 	}
 	
-//====================================================================================================================================== 게시글 등록/수정		
-	@PostMapping(value="/member/boardRegister")
-	public String boardWrite(BoardDTO boardDTO) throws Exception{		
-
-		if(boardDTO.getWriter() == null) {
-			boardDTO.setWriter("작성자");
+//==================================================================================================================== 게시글 상세보기	
+	@GetMapping(value="/board/boardView")
+	public String boardView(@ModelAttribute("params") BoardDTO params,@RequestParam(value="bno", required=false) Integer bno, Model model) throws Exception{
+		if(bno==null) {
+			return "redirect:/board/boardList";
 		}
-		boardDTO.setBoardType("notice");
+		BoardDTO board = boardService.selectBoardDetail(bno);
+		model.addAttribute("board", board);
+		return "board/boardView";
+	}
+	
+//===================================================================================================================== 게시글 등록
+	@PostMapping(value="/board/boardRegister")
+	public String boardRegister(BoardDTO boardDTO) throws Exception{		
 		boardService.insertBoard(boardDTO);
-		return "redirect:/member/boardList";
+		return "redirect:/board/boardList";
 	}
 	
-	@GetMapping(value="/member/boardRegister")
-	public String boardRegister(@RequestParam(value="bno", required=false) Integer bno, Model model) throws Exception {
-		if(bno == null) { // 글 새로 등록
-			model.addAttribute("board",new BoardDTO());
-		}else {			 // 글 수정
-			BoardDTO board = boardService.selectBoardDetail(bno);
-			if(board == null) {
-				return "redirect:/member/boardList";
-			}
-			model.addAttribute("board", board);
-		}
-		return "member/boardWrite";
-		
+	@GetMapping(value="/board/boardWrite")
+	public String boardWrite(Model model) throws Exception {
+
+		return "board/boardWrite";
 	}
-//====================================================================================================================================== 게시글 목록	
-	@GetMapping(value="/member/boardList")
+//==================================================================================================================== 게시글 목록	
+	@GetMapping(value="/board/boardList")
 	public String boardList(@ModelAttribute("params") BoardDTO boardDTO, Model model, BoardTypeDTO boardTypeDTO) throws Exception {
-		System.out.println("/member/boardList");
+		System.out.println("/board/boardList");
 		List<BoardDTO> list = boardService.selectBoardList(boardDTO);
 		System.out.println("list : "+list);
 		model.addAttribute("list", list);
 	    
 		List<BoardTypeDTO> board_type = boardTypeService.selectBoardTypeList();
 		model.addAttribute("board_type", board_type);
-		return "member/boardList";
+		return "board/boardList";
 		
 	}
 		
